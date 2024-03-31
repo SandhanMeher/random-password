a76dfde031db37dbe4eb4ab0512191e6d99d5793
Sandhan Meher <codewithsandhan@gmail.com>
2024-04-01 01:05:00 +0530

    passwords

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const path=require("path")
+
+const app=express();
+
+app.use(express.static("dist"))
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"dist","index.html"))
+})
+
+
+app.listen(3000,()=>{
+    console.log("listening at 3000")
+})
